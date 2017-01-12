92402677bd697b855aa407e067738b69d4933648
Noah Berman <noah@noahberman.org>
2017-01-12 15:52:13 -0700

    styling fixed and email separate

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -2,9 +2,11 @@ require 'wikipedia'
 
 class LandingController < ApplicationController
   def index
-		date_randomized = rand(1..100000)
-		@date = date_randomized.days.ago.strftime("%b %-d")		
+		date_randomized = rand(1..366)
+		@date = date_randomized.days.ago.strftime("%B %-d")
+		@time = Time.now.strftime("%H:%M")
 		article = Wikipedia.find("#{@date}")
 		@facts = article.summary
+		@image = article.image_descriptionurls[0]
 	end
 end
