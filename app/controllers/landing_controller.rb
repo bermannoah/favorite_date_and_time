e203faff8e6e81e2294df17ee11bc5cc118a11ba
Noah Berman <noah@noahberman.org>
2017-01-12 15:36:31 -0700

    add email button to footer and fix text styling

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,6 +1,10 @@
+require 'wikipedia'
+
 class LandingController < ApplicationController
   def index
 		date_randomized = rand(1..100000)
-		@date = date_randomized.days.ago.strftime("%b %-d %Y")		
-  end
+		@date = date_randomized.days.ago.strftime("%b %-d")		
+		article = Wikipedia.find("#{@date}")
+		@facts = article.summary
+	end
 end
